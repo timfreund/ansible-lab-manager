b6a352367237da65eec5a498a0ab0266862f3691
Tim Freund <tim@freunds.net>
2020-01-10 11:11:36 -0500

    Config management

---
+++ alm/__init__.py
@@ -0,0 +1,48 @@
+import os
+import yaml
+
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+
+def load_config_file(file_path):
+    c = {}
+    try:
+        with open(file_path, "r") as config_file:
+            c = yaml.load(config_file)
+    except Exception as e:
+        print(e)
+    return c
+
+def load_config_text(config_text):
+    return yaml.load(config_text)
+
+def load_environment_variables():
+    c = {}
+    for k, v in os.environ.items():
+        if k.startswith('ALM_'):
+            ck = k.replace('ALM_', '').lower()
+            c[ck] = v
+    return c
+
+def merge_configs(priority1, priority2):
+    c = {}
+    c.update(priority2)
+    c.update(priority1)
+    return c
+
+def autoload_configuration():
+    defaults = load_config_text(pkg_resources.read_text('alm', 'defaults.yml'))
+    cwd = load_config_file(os.path.sep.join([os.getcwd(), ".ansible-lab-manager.yml"]))
+    home = load_config_file(os.path.sep.join([os.path.expanduser("~"), ".ansible-lab-manager.yml"]))
+    env = load_environment_variables()
+
+    # TODO that's pretty ugly, perhaps merge_configs should take an arbitrary
+    # number of configuration dictionaries
+    c = merge_configs(home, defaults)
+    c = merge_configs(cwd, c)
+    c = merge_configs(env, c)
+
+    return c
