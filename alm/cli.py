056d36cf2c9f3547554ea89188325a254f7b68cb
Tim Freund <tim@freunds.net>
2020-01-10 11:12:52 -0500

    Allow for configurable docker images

--- alm/cli.py
+++ alm/cli.py
@@ -1,5 +1,6 @@
 import click
 
+from alm import autoload_configuration
 from ansible.parsing.dataloader import DataLoader
 from ansible.inventory.manager import InventoryManager
 from jinja2 import Environment, PackageLoader
@@ -7,12 +8,15 @@ from jinja2 import Environment, PackageLoader
 @click.command()
 @click.option('--inventory', default="./hosts", prompt='Ansible inventory file')
 def ansible_lab_manager(inventory):
+    config = autoload_configuration()
+    template_context = config.copy()
+
     ansible_loader = DataLoader()
-    inventory = InventoryManager(loader=ansible_loader, sources=inventory)
+    template_context['inventory'] = InventoryManager(loader=ansible_loader, sources=inventory)
     
     jinjaenv = Environment(loader=PackageLoader('alm', 'templates'))
     compose_template = jinjaenv.get_template('docker-compose.yml.j2')
-    compose_rendered = compose_template.render(dict(inventory=inventory))
+    compose_rendered = compose_template.render(template_context)
     print(compose_rendered)
     # for h in inventory.hosts:
     #     print(h)
